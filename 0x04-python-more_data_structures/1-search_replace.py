2474cf5a2921ace4fc31c8acb5a54865bf439a47
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-10 17:10:46 +0000

    if my-list is empty

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
 
     if not my_list:
-        return my_list
+        return new_list
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
