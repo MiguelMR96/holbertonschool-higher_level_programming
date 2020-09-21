ecc5a83437c919db9058d2afc95714a3ef5d49a2
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-21 18:02:53 +0000

    Third task

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        if b != 0:
+            result = a / b
+        else:
+            result = None
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
