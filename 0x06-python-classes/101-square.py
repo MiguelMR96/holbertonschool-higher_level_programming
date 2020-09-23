02a3d99b3a6126b9d124b276d15a1ed6ea347dd5
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-23 05:05:01 +0000

    __str__ method

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -56,14 +56,14 @@ class Square:
     def __str__(self):
         if self.__size == 0:
             print()
-            return
-        for y in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for x in range(self.__position[0]):
-                print(" ", end="")
-            for j in range(self.__size):
-                print("#", end="")
-            if i != self.__size - 1:
+        else:
+            for y in range(self.__position[1]):
                 print()
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                if i != self.__size - 1:
+                    print()
         return ""
