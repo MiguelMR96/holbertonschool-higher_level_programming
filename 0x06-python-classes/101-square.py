939709a452b111c0ec2d690f84f2a7e4725802d1
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-23 04:47:20 +0000

    __str__ method

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Square with size private attribute
+"""
+
+
+class Square:
+    """Square with size private attribute
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif int(value) < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for y in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for x in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            if i != self.__size - 1:
+                print()
+
+    def __str__(self):
+        string = self.my_print()
+        return ""
