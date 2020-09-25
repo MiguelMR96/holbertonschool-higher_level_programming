39bb3ae26a138c7ad01d26d71f47d26fa8326134
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-25 23:55:11 +0000

    Adding documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+""" add two integers
+"""
+
+
 def add_integer(a, b=98):
+    """[summary]
+
+    Args:
+        a (int): number
+        b (int, optional): Number. Defaults to 98.
+
+    Raises:
+        TypeError: when a is not a number
+        TypeError: when b is not a number
+
+    Returns:
+        a + b: result af adding two numbers
+    """
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
