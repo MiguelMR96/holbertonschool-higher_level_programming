de09e9e014cb478cfb9b55f1ab6b1212070d4afd
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-26 02:03:32 +0000

    Pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,6 +5,7 @@ import unittest
 import pep8
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """
     Unit test
@@ -14,8 +15,11 @@ class TestMaxInteger(unittest.TestCase):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(["6-max_integer.py"])
-        self.assertEqual(result.total_errors, 0, "Found code style errors (and warnings).")    
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
+
     def test_not_list(self):
         self.assertEqual(max_integer("Miguel"), 'u')
