7c0965c64b4887a0892aea44cd97ede31bcf6cc5
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-09-29 21:00:58 +0000

    uploading advanced task and some corrections of others

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string.append("Holberton")
-    return ", ".join(string)
+def magic_string(lists=[]):
+    lists.append("Holberton")
+    return ", ".join(lists)
