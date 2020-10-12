03f0639e02fde5fda9a80161c3c3a392c8adad43
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-10-12 18:03:39 +0000

    backup for first steps with classes working fine so far

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -23,6 +23,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -30,5 +34,67 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def height(self):
-        self.__heigth = heigth
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
+
+    def area(self):
+        """ Rectangle's area
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """ Prints rectangle hashes in sandard output
+        """
+        for vertical in range(self.__y):
+            print()
+        for h in range(self.__height):
+            for horizontal in range(self.__x):
+                print(' ', end='')
+            for w in range(self.__width):
+                print('#', end='')
+            print()
+
+    def __str__(self):
+        return '[{}] ({}) {}/{} - {}/{}'.format(self.__class__.__name__,
+                                                self.id, self.__x, self.__y,
+                                                self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """ Update attributes
+        """
+        attr = ['id', 'width', 'height', 'x', 'y']
+
+        if args:
+            for i in range(len(args)):
+                setattr(self, attr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,26 +5,25 @@
 
 import unittest
 import pep8
-from models.base import Base
+from models.rectangle import Rectangle
 
 
-class testBase(unittest.TestCase):
+class testRectangle(unittest.TestCase):
     """ Test base class
 
     """
     def test_pep8(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(["models/base.py"])
+        result = pep8style.check_files(["models/rectangle.py"])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_Base(self):
+    def test_Rectangle(self):
         self.assertTrue(True)
 
-    def test_base(self):
+    def test_rectangle(self):
         self.assertTrue(True)
 
-    def test_id_1(self):
-        obj1 = Base()
-        self.assertEqual(obj1.id, 1)
+    def test_area(self):
+        self.assertEqual(Rectangle())
