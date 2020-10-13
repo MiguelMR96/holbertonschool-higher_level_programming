501bb94ed7eeced039884d7536ee2b7756d2588f
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-10-13 05:16:26 +0000

    json string representation of an object

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Returns in a json string representation
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ Returns in a json string representation
+
+    Args:
+        my_obj (obj): object to be serializated in
+        json string representation
+    """
+    return json.dumps(my_obj)
