aa23fe97d265da06d7266b02ae23b3837e387ee0
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-02 22:03:48 -0500

    Printing using loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop.
+
+const arrayStrings = ['C is fun',
+  'Python is cool',
+  'Javascript is amazing'];
+
+for (let step = 0; step < 3; step++) {
+  console.log(arrayStrings[step]);
+}
