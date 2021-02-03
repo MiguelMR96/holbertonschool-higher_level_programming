6638c1749101d3f2d94b565d964183f774cab117
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-02 21:22:49 -0500

    No typeof needed, comparison with ===

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,4 +4,4 @@
 // expression. Info about ternary structure:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
 
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+console.log(process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
