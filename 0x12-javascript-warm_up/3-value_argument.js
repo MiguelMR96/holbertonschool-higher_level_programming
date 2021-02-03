2214493a79b17135a89ea8863283208b29223335
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-02 21:06:49 -0500

    Task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Print the argument passed when program run. Using ternary
+// expression. Info about ternary structure:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
