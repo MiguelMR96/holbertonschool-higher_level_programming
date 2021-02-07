f982df796fc50749269cc26ac0ff65c618dc3b19
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-06 23:25:34 -0500

    Task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""This module makes a query from MySQL
 """
-
+Script that prints all City objects from the database hbtn_0e_14_usa
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    from sqlalchemy import create_engine, text
-    from sqlalchemy.orm import sessionmaker
-    from sys import argv
-    from model_state import State, Base
-    from model_city import City
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State, City).filter(City.state_id == State.id)\
-                                      .order_by(City.id).all()
-    for state, city in query:
-        print("{:s}: ({:d}) {:s}".format(state.name, city.id, city.name))
+    for state, city in session.query(State, City).filter(
+            City.state_id == State.id).order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-"""This module defines a City class"""
+"""
+contains the class definition of a City.
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base
+from model_state import Base
 
 
 class City(Base):
-    """[summary]
-
-    Args:
-        Base ([type]): [description]
-    """
-    __tablename__ = "cities"
-    id = Column(
-        Integer,
-        primary_key=True,
-        nullable=False
-    )
-    name = Column(
-        String(128),
-        nullable=False
-    )
-    state_id = Column(
-        Integer,
-        ForeignKey("states.id"),
-        nullable=False
-    )
+    """Class City"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
