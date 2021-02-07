d5f5b260905d37bb13967a4ae1905f87b108408c
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-06 22:09:16 -0500

    Advanced task

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 // Increments and calls a function
-exports.addMeMaybe = function (number, theFunction) {
-  let j = 0;
-  for (let i = 0; i <= number; i++) {
-    j = j + 1;
-  }
-  theFunction(j);
+
+exports.addMeMaybe = function (num, theFunction) {
+  theFunction(num + 1);
 };
