43c1fbbaf48da5f437be0ad71dd7a478b18c5291
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-06 23:08:04 -0500

    Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""[summary]
-"""
-if __name__ == "__main__":
-    import sys
-    import sqlalchemy
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
+"""This module makes a query to MySQL"""
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]))
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine, text
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import State, Base
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for i in session.query(State).filter(State.name.like(sys.argv[4])).all():
-        try:
-            print("{}".format(state.id))
-        except:
-            print("Not found")
+    rows = session.query(State.id, State.name).from_statement(text(
+        "SELECT id, name "
+        "FROM states "
+        "WHERE name=:input_name "
+    ).bindparams(input_name=argv[4])).all()
+    if rows == []:
+        print("Not found")
+    else:
+        for id, name in rows:
+            print("{:d}".format(id))
     session.close()
