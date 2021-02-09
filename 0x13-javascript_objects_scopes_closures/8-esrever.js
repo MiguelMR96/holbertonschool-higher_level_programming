4b97afbf5eaa97a0ed4c3a73d1674cd60000ea88
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-08 23:35:19 -0500

    8 task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 // Returns reversed version of a list
-let i = 0;
-exports.logMe = function (item) {
-  console.log(i++ + ': ' + item);
+exports.esrever = function (str) {
+  const reversed = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+  return reversed;
 };
