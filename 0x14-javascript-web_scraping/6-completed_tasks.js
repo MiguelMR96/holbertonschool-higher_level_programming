51ecff4892875b6edb5697dbdad31f4a594bdf65
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-02-09 22:34:46 -0500

    Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Display completed tasks
+
+const re = require('request');
+
+re.get(process.argv[2], function (error, res, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    const dataList = {};
+    for (const i of JSON.parse(data)) {
+      if (i.completed) {
+        if (!dataList[i.userId]) {
+          dataList[i.userId] = 1;
+        } else {
+          dataList[i.userId] += 1;
+        }
+      }
+    }
+    console.log(dataList);
+  }
+});
